cd1e4c5e59dd9ff5119d182898ddbd8d5b5af578
Akira Moroo <retrage01@gmail.com>
2019-03-05 16:02:06 +0900

    lib/nolibc: Add STD{IN,OUT,ERR}_FILENO
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- lib/nolibc/include/unistd.h
+++ lib/nolibc/include/unistd.h
@@ -48,6 +48,10 @@ extern "C" {
 #include <nolibc-internal/shareddefs.h>
 
 #if CONFIG_LIBVFSCORE
+#define STDIN_FILENO  0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
+
 int close(int fd);
 ssize_t write(int fd, const void *buf, size_t count);
 ssize_t read(int fd, void *buf, size_t count);
