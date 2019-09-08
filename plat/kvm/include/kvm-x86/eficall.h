a98d0af53d1050b35266ef068de08d52422369fb
Akira Moroo <retrage01@gmail.com>
2019-09-08 17:16:10 +0900

    Add eficall5 wrapper
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- plat/kvm/include/kvm-x86/eficall.h
+++ plat/kvm/include/kvm-x86/eficall.h
@@ -7,5 +7,6 @@ int efi_call1(void *func, void *arg1);
 int efi_call2(void *func, void *arg1, void *arg2);
 int efi_call3(void *func, void *arg1, void *arg2, void *arg3);
 int efi_call4(void *func, void *arg1, void *arg2, void *arg3, void *arg4);
+int efi_call5(void *func, void *arg1, void *arg2, void *arg3, void *arg4, void *arg5);
 
 #endif /* EFICALL_H_ */
